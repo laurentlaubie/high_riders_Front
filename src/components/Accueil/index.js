4a60079ca81221f60cf172a02b44d0c77a126921
MohammedKasbi <mohammed.kasbi45@gmail.com>
2021-09-20 17:06:58 +0200

    feat: adding-jsx-for-homepage-and-one-card

--- src/components/Accueil/index.js
+++ src/components/Accueil/index.js
@@ -1,5 +1,31 @@
+// == Import persos
+import background from 'src/assets/images/background.jpg';
+import fondCard from 'src/assets/images/fondCard.jpg';
+import './style.scss';
+
 const Accueil = () => (
-  <div>Accueil</div>
+  <div>
+    <img src={background} alt="Illustration" className="home__background" />
+    <div>
+      <h2>Bienvenue sur High Riders</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam at lacus sit amet
+        felis rutrum sodales sed eget est. Praesent consequat, eros in blandit pulvinar,
+        lorem magna tincidunt augue, at volutpat tortor ligula ac augue.
+      </p>
+    </div>
+    <div>
+      <h1>Derniers évènements</h1>
+      <div className="card">
+        <img src={fondCard} alt="Event 1" className="card__img" />
+        <span>Nom de la ville</span>
+        <span>15 Oct. 2021</span>
+        <h2>Nom de l'evenement</h2>
+        <h3>Disciplines pratiquées : </h3>
+        <span>VTT</span>
+        <p>Lorem ipsum dolor sit amet, adipiscing elit. Nullam at lacus sit amet felis...</p>
+      </div>
+    </div>
+  </div>
 );
 
 export default Accueil;
