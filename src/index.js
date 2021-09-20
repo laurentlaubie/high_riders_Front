249f607a52248e3a9a74ddfaaf4c210bc12afd41
MohammedKasbi <mohammed.kasbi45@gmail.com>
2021-09-20 11:58:27 +0200

    react-model

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// == Import : npm
+import { render } from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// == Import : local
+// Composants
+import App from 'src/components/App';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// == Render
+// 1. Élément React racine (celui qui contient l'ensemble de l'app)
+//    => crée une structure d'objets imbriqués (DOM virtuel)
+const rootReactElement = <App />;
+// 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
+const target = document.getElementById('root');
+// 3. Déclenchement du rendu de React (virtuel) => DOM (page web)
+render(rootReactElement, target);
