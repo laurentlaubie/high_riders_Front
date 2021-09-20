7388d01dca7c35fa42c399dd3ec9b6c18d935076
Prudhomme Pascal <skalll@hotmail.fr>
2021-09-20 15:29:01 +0200

    feat: addLogo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,9 @@
 import { NavLink } from 'react-router-dom';
+import highridersLogo from './highridersLogo.png';
 
 const Header = () => (
   <div>
+    <img src={highridersLogo} alt="logo" />
     <NavLink exact to="/">Accueil</NavLink>
     <NavLink to="/spots">Spots</NavLink>
     <NavLink to="/evenements">Évènements</NavLink>
