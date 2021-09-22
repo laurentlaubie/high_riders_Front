701490dc38c0a7f5080475c2ada613fb811c28d2
Prudhomme Pascal <skalll@hotmail.fr>
2021-09-22 13:12:19 +0200

    feat:register-page

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,5 +1,25 @@
+import './style.scss';
+import avatar from './avatar.png';
+
 const Register = () => (
-  <div>Register</div>
+  <div className="register">
+    <h1 className="register__title">Créer votre profil</h1>
+    <img className="register__avatar" src={avatar} alt="avatar" />
+    <input className="register__form" placeholder="nom ..." />
+    <input className="register__form" placeholder="prenom ..." />
+    <input className="register__form" placeholder="pseudo ..." />
+    <input className="register__form" placeholder="email ..." />
+    <input className="register__form" placeholder="mot de passe ..." />
+    <input className="register__form" placeholder="description ..." />
+    <input className="register__form" placeholder="ville ..." />
+    <input className="register__form" placeholder="équipement ..." />
+    <select className="register__selector">
+      <option>discipline</option>
+      <option>b</option>
+      <option>c</option>
+    </select>
+    <button className="register__button" type="submit">Envoyer</button>
+  </div>
 );
 
 export default Register;
