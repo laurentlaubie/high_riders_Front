a7f7dd449ee3257d62abedebede46dfd155c4682
MohammedKasbi <mohammed.kasbi45@gmail.com>
2021-09-24 10:41:24 +0200

    fix: improvement of spotlist --DONE

--- src/components/Spots/index.js
+++ src/components/Spots/index.js
@@ -16,31 +16,24 @@ const spotList = () => (
       <select className="spotList__filter__selector">
         <option className="spotList__filter__selector--category">Disciplines</option>
       </select>
-      <input className="spotList__filter--search" type="search" placeholder="Recherche de spots ..."/>
+      <input className="spotList__filter--search" type="search" placeholder="Recherche de spots ..." />
     </div>
     <div className="spotList__image">
       <img src={map} alt="map" className="spotList__image--img" />
     </div>
-    <div className="spotList__cards">  
+    <div className="spotList__cards">
       <div className="spotList__list">
-        <h1>Meilleurs spots</h1>
+        {/* <h1>Spots</h1> */}
         <div className="spotList__list__elem">
           {data.map((item) => (
             <Card key={item.id} {...item} />
           ))}
-        </div>
-      </div>
-      <div className="spotList__list">
-        <h1>Derniers spots ajoutés</h1>
-        <div className="spotList__list__elem">
           {data.map((item) => (
             <Card key={item.id} {...item} />
           ))}
-        </div>
-      </div>
-      <div className="spotList__list">
-        <h1>Derniers évènements</h1>
-        <div className="spotList__list__elem">
+          {data.map((item) => (
+            <Card key={item.id} {...item} />
+          ))}
           {data.map((item) => (
             <Card key={item.id} {...item} />
           ))}
