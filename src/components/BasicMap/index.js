202dfd64691da1b185cfcba1e6c8996ebff7c611
Prudhomme Pascal <skalll@hotmail.fr>
2021-09-25 10:56:54 +0200

    feat:add-map--DONE

---
+++ src/components/BasicMap/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import './style.scss';
+
+const BasicMap = () => (
+  <MapContainer center={[46.50, 2.00]} zoom={4.8} scrollWheelZoom={true}>
+    <TileLayer
+      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={[46.00, 2.00]}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
+);
+
+export default BasicMap;
