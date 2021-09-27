ced7ab408d5b68e03982cbe76466d7afa398e6c6
Prudhomme Pascal <skalll@hotmail.fr>
2021-09-27 13:18:24 +0200

    feat:map-markers-data--DONE

--- src/components/BasicMap/index.js
+++ src/components/BasicMap/index.js
@@ -1,23 +1,23 @@
 import React, { useState, useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import spots from 'src/data';
 import './style.scss';
 
 const BasicMap = () => {
   const [center, setCenter] = useState({ lat: 46.50, lng: 2.00 });
   const ZOOM_LEVEL = 9;
-  const ZOOM_SCROLL = true;
   const mapRef = useRef();
 
   return (
-    <MapContainer center={center} zoom={ZOOM_LEVEL} scrollWheelZoom={ZOOM_SCROLL} ref={mapRef}>
+    <MapContainer center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {spots.map((spot, id) => (
+      {spots.map((spot) => (
         <Marker 
           position={[spot.latitude, spot.longitude]}
-          key={id}
+          key={spot.id}
         >
           <Popup>
             { spot.title }
