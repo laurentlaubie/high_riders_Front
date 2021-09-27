0ac265f013321d0773f35012c5bf6cd966819b24
Prudhomme Pascal <skalll@hotmail.fr>
2021-09-27 12:10:08 +0200

    feat:dynamic-markers--DOING

--- src/components/BasicMap/index.js
+++ src/components/BasicMap/index.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './style.scss';
 
-const BasicMap = () => (
-  <MapContainer center={[46.50, 2.00]} zoom={4.8} scrollWheelZoom={true}>
-    <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={[46.00, 2.00]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
-);
+const BasicMap = () => {
+  const [center, setCenter] = useState({ lat: 46.50, lng: 2.00 });
+  const ZOOM_LEVEL = 9;
+  const ZOOM_SCROLL = true;
+  const mapRef = useRef();
 
+  return (
+    <MapContainer center={center} zoom={ZOOM_LEVEL} scrollWheelZoom={ZOOM_SCROLL} ref={mapRef}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {spots.map((spot, id) => (
+        <Marker 
+          position={[spot.latitude, spot.longitude]}
+          key={id}
+        >
+          <Popup>
+            { spot.title }
+          </Popup>
+        </Marker> 
+      ))}
+    </MapContainer>
+  );
+}
 export default BasicMap;
