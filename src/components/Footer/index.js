f9722e983af845e0f04b04f93bd145bbeaac0dff
Otsutsuki <atakan.bekki@gmail.com>
2021-09-30 14:27:40 +0200

    feat: footer link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,13 @@
+import { NavLink } from 'react-router-dom';
+import './style.scss';
+
 const Footer = () => (
-  <div>Footer</div>
+  <div className="footer">
+    <NavLink className="footer__nav__item" exact to="/LegalNotice">Mentions légales</NavLink>
+    <NavLink className="footer__nav__item" to="/AboutUs">A propos de nous</NavLink>
+    <NavLink className="footer__nav__item" to="/SiteMap">Plan de site</NavLink>
+    <NavLink className="footer__nav__item" to="/ContactUs">Nous contacter</NavLink>
+  </div>
 );
 
 export default Footer;
