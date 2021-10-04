e7cbdee93bde5d2c9083a35af5fb5ec1e1a4d84e
MohammedKasbi <mohammed.kasbi45@gmail.com>
2021-10-04 08:38:23 +0200

    feat: events page with dynamic data --DONE

--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -1,46 +1,61 @@
 // == Import composants
 import Card from 'src/components/Card';
-import data from 'src/data';
+// import data from 'src/data';
 import BasicMap from 'src/components/BasicMap';
 
 // == Import persos
 import './style.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
-const Events = () => (
-  <div className="eventList">
-    <h1 className="eventList__title">Liste des evènements</h1>
-    <div className="eventList__filter">
-      <select className="eventList__filter__selector">
-        <option className="eventList__filter__selector--county">Département</option>
-      </select>
-      <select className="eventList__filter__selector">
-        <option className="eventList__filter__selector--category">Disciplines</option>
-      </select>
-      <input className="eventList__filter--search" type="search" placeholder="Recherche d'evènements ..." />
-    </div>
-    <div className="eventList__image">
-      <BasicMap />
-    </div>
-    <div className="eventList__cards">
-      <div className="eventList__list">
-        {/* <h1>Evènements</h1> */}
-        <div className="eventList__list__elem">
-          {data.map((item) => (
-            <Card key={item.id} {...item} typeCard="events" />
+const Events = () => {
+  const dispatch = useDispatch();
+  const eventsList = useSelector((state) => state.events.eventsList);
+  const eventsDepar = useSelector((state) => state.events.eventsDepar);
+  const eventsCateg = useSelector((state) => state.events.eventsCate);
+
+  console.log(eventsDepar);
+  console.log(eventsCateg);
+
+  useEffect(() => {
+    dispatch({
+      type: 'FETCH_EVENTS_DATA',
+    });
+  }, []);
+
+  return (
+    <div className="eventList">
+      <h1 className="eventList__title">Liste des evènements</h1>
+      <div className="eventList__filter">
+        <select className="eventList__filter__selector">
+          <option className="eventList__filter__selector--county">Département</option>
+          {eventsDepar.map((elem) => (
+            <option key={elem.id} value={elem.title}>{elem.title}</option>
           ))}
-          {data.map((item) => (
-            <Card key={item.id} {...item} typeCard="events" />
+        </select>
+        <select className="eventList__filter__selector">
+          <option className="eventList__filter__selector--category">Disciplines</option>
+          {eventsCateg.map((elem) => (
+            <option key={elem.id} value={elem.title}>{elem.title}</option>
           ))}
-          {data.map((item) => (
-            <Card key={item.id} {...item} />
-          ))}
-          {data.map((item) => (
-            <Card key={item.id} {...item} typeCard="events" />
-          ))}
+        </select>
+        <input className="eventList__filter--search" type="search" placeholder="Recherche d'evènements ..." />
+      </div>
+      <div className="eventList__image">
+        <BasicMap />
+      </div>
+      <div className="eventList__cards">
+        <div className="eventList__list">
+          <h1>Tous les évènements disponibles en France</h1>
+          <div className="eventList__list__elem">
+            {eventsList.map((item) => (
+              <Card key={item.id} {...item} typeCard="events" />
+            ))}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Events;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 
 import homeReducer from './home';
 import spotsReducer from './spots';
+import eventsReducer from './events';
 import searchReducer from './search';
 import userReducer from './user';
 
 const rootReducer = combineReducers({
   home: homeReducer,
   spots: spotsReducer,
+  events: eventsReducer,
   search: searchReducer,
   user: userReducer,
 });
